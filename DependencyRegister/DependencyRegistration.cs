005fd880741f901c51edfea7177213031ef72e78
Paul Sorauer <paul@codered.com.au>
2020-10-21 11:23:12 +1100

    Initial Package Creation
    Created generic 'DependencyRegistration' wrapper
    Added extension method

---
+++ DependencyRegister/DependencyRegistration.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeRed.Extensions.DependencyInjection
+{
+    public class DependencyRegistration<TScope, TDependency>
+    {
+        public TDependency Dependency { get; }
+
+        public DependencyRegistration(TDependency dependency)
+        {
+            Dependency = dependency;
+        }
+    }
+}
---
+++ DependencyRegister/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeRed.Extensions.DependencyInjection
+{
+    public static class Extensions
+    {
+        public static TDependency GetRequiredRegisteredService<TScope, TDependency>(this IServiceProvider serviceProvider)
+        {
+            return serviceProvider.GetRequiredService<DependencyRegistration<TScope, TDependency>>().Dependency;
+        }
+    }
+}
